32f398fad65557df2338b86ea1c6686d11950cff
Krishnamoorthi KS <krishdataeng@gmail.com>
2025-02-13 16:00:28 +0530

    filder structer added"

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Krishnamoorthi KS',
+    author_email= 'krishnamoorthi.somamoorthi@nulogic.io',
+    packages= find_packages(),
+    install_requires = [],
+
+)
